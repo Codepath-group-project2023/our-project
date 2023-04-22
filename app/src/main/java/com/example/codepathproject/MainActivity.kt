446f3e7a0cfb806c257fb631e759f83c7f19b497
Kazi Anwar <kazisameen2702@gmail.com>
2023-04-22 18:15:56 -0400

    Modified activity_main.xml to incorporate recycler view

--- app/src/main/java/com/example/codepathproject/MainActivity.kt
+++ app/src/main/java/com/example/codepathproject/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.codepathproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var imagelist: MutableList<String>
+    private lateinit var userlist: MutableList<String>
+    private lateinit var categorylist: MutableList<String>
+    var input = ""
+    private lateinit var rvRecipes: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvRecipes = findViewById(R.id.recipe_list)
+
     }
 }
