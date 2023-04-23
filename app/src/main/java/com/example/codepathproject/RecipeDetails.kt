085cab7457ca4329c82c6dbf5281034d19305b06
kinor <kinoriiyuu@gmail.com>
2023-04-23 16:18:32 -0400

    implemented loading a new activity on click

--- app/src/main/java/com/example/codepathproject/RecipeDetails.kt
+++ app/src/main/java/com/example/codepathproject/RecipeDetails.kt
@@ -2,15 +2,13 @@ package com.example.codepathproject
 
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class RecipeDetails : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.recipe_details)
+        setContentView(R.layout.activity_recipe_details)
 
 
     }
