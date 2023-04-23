8513824541d172b244a2fb4cdfe904761ef74f35
kinor <kinoriiyuu@gmail.com>
2023-04-22 20:44:28 -0400

    test api client

--- app/src/main/java/com/example/codepathproject/MainActivity.kt
+++ app/src/main/java/com/example/codepathproject/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.codepathproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import okhttp3.Headers
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +21,28 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         rvRecipes = findViewById(R.id.recipe_list)
+        imagelist = mutableListOf()
+        userlist = mutableListOf()
+        categorylist = mutableListOf()
 
+        configureCategories()
+    }
+    private fun configureCategories(){
+        val client = AsyncHttpClient()
+
+        client["https://www.themealdb.com/api/json/v1/1/categories.php", object : JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Category Error",errorResponse)
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers, json: JsonHttpResponseHandler.JSON) {
+                Log.d("Category Success", "categories")
+            }
+        }]
     }
 }
